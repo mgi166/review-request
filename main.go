85415196005dbd42c6f96eae0065f4eaee8215f7
mgi166 <skskoari@gmail.com>
2017-02-05 12:43:24 +0900

    Create original help template.

--- main.go
+++ main.go
@@ -21,6 +21,40 @@ import (
     "github.com/urfave/cli"
 )
 
+const helpTemplate = `
+NAME:
+   {{.Name}}{{if .Usage}} - {{.Usage}}{{end}}
+
+USAGE:
+   {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}{{if .Version}}{{if not .HideVersion}}
+
+ARGUMENTS
+   Requires just two arguments.
+
+   * PULL_REQUEST_URL: github pull request url. e.g) https://github.com/rails/rails/pull/1
+   * PHASE: Review phase. e.g) 1, 2
+
+EXAMPLE:
+   review https://github.com/mgi166/rails-showcase/pull/1 1
+
+VERSION:
+   {{.Version}}{{end}}{{end}}{{if .Description}}
+
+DESCRIPTION:
+   {{.Description}}{{end}}{{if len .Authors}}
+
+AUTHOR{{with $length := len .Authors}}{{if ne 1 $length}}S{{end}}{{end}}:
+   {{range $index, $author := .Authors}}{{if $index}}
+   {{end}}{{$author}}{{end}}{{end}}{{if .VisibleCommands}}
+
+GLOBAL OPTIONS:
+   {{range $index, $option := .VisibleFlags}}{{if $index}}
+   {{end}}{{$option}}{{end}}{{end}}{{if .Copyright}}
+
+COPYRIGHT:
+   {{.Copyright}}{{end}}
+`
+
 func main () {
 	app := cli.NewApp()
 	app.Name = "review"
